4d539251f67d793c104d41625750a674bd21fbc5
muraliweb9 <muraliweb@hotmail.com>
2023-05-23 18:10:54 +0100

    Initial

--- src/test/java/com/interview/carhire/proto/CarHireProtoApplicationTests.java
+++ src/test/java/com/interview/carhire/proto/CarHireProtoApplicationTests.java
@@ -2,12 +2,12 @@ package com.interview.carhire.proto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CarHireProtoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
